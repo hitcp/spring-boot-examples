c2bbfd1d6d8fa9898f83495e99fc47aac9fedf33
刘邵宇 <455028117@qq.com>
2022-08-19 15:51:01 +0800

    spring-boot-elasticsearch

--- spring-boot-elasticsearch-examples/src/main/java/org/microframework/elasticsearch/ElasticsearchApplication.java
+++ spring-boot-elasticsearch-examples/src/main/java/org/microframework/elasticsearch/ElasticsearchApplication.java
@@ -1,15 +1,15 @@
-package org.microframework.elasticsearch;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-/**
- * @author Shaoyu Liu
- * @date 2022-08-19
- */
-@SpringBootApplication
-public class ElasticsearchApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ElasticsearchApplication.class, args);
-    }
-}
+//package org.microframework.elasticsearch;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+///**
+// * @author Shaoyu Liu
+// * @date 2022-08-19
+// */
+//@SpringBootApplication
+//public class ElasticsearchApplication {
+//    public static void main(String[] args) {
+//        SpringApplication.run(ElasticsearchApplication.class, args);
+//    }
+//}
