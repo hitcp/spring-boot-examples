86eab9c214ac01dff48e57c55a6eae97ea414495
刘邵宇 <455028117@qq.com>
2022-09-09 14:00:24 +0800

    webflux http

---
+++ spring-boot-webflux-examples/src/main/java/org/microframework/webflux/config/WebConfig.java
@@ -0,0 +1,37 @@
+package org.microframework.webflux.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.reactive.HandlerMapping;
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.server.WebHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Http方式进行通信
+ *
+ * @author Shaoyu Liu --- website: <a href="http://hitcp.cn">Hi TCP</a>
+ * @date 2022-09-09
+ */
+@Configuration
+public class WebConfig {
+    /**
+     * Request Method: GET
+     * Request URL: http://localhost:8080/web/echo
+     *
+     * @param webHandler
+     * @return
+     */
+    @Bean
+    public HandlerMapping simpleWebHandlerMapping(final WebEchoHandler webHandler) {
+        final Map<String, WebHandler> map = new HashMap<>();
+        map.put("/web/echo", webHandler);
+        final SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
+        mapping.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        mapping.setUrlMap(map);
+        return mapping;
+    }
+}
---
+++ spring-boot-webflux-examples/src/main/java/org/microframework/webflux/config/WebEchoHandler.java
@@ -0,0 +1,36 @@
+package org.microframework.webflux.config;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.microframework.webflux.entity.User;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebHandler;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author Shaoyu Liu --- website: <a href="http://hitcp.cn">Hi TCP</a>
+ * @date 2022-09-09
+ */
+@Component
+public class WebEchoHandler implements WebHandler {
+
+    @Override
+    public Mono<Void> handle(ServerWebExchange exchange) {
+        ServerHttpResponse response = exchange.getResponse();
+        DataBufferFactory bufferFactory = response.bufferFactory();
+        ObjectMapper objectMapper = new ObjectMapper();
+        User user = new User("张三");
+        DataBuffer wrap = null;
+        try {
+            wrap = bufferFactory.wrap(objectMapper.writeValueAsBytes(user));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        DataBuffer finalWrap = wrap;
+        return response.writeWith(Mono.fromSupplier(() -> finalWrap));
+    }
+}
