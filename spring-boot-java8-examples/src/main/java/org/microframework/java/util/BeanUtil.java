36718d41fc291f55b3b48950db35884f85fe4e4c
刘邵宇 <455028117@qq.com>
2022-09-28 17:51:16 +0800

    init BeanUtil

---
+++ spring-boot-java8-examples/src/main/java/org/microframework/java/util/BeanUtil.java
@@ -0,0 +1,16 @@
+package org.microframework.java.util;
+
+/**
+ * @author Shaoyu Liu --- website: <a href="http://hitcp.cn">Hi TCP</a>
+ * @date 2022-09-28
+ */
+public class BeanUtil {
+
+    //    public static <T> T convertObject2(Object source, Class<T> target) throws InstantiationException, IllegalAccessException {
+//        T t = target.newInstance();
+//        return t;
+//    }
+
+    private BeanUtil() {
+    }
+}
