7457dc7a1948c6998332422a9a1da9861542847d
刘邵宇 <455028117@qq.com>
2022-12-05 14:44:12 +0800

    delete manually version dependency

--- spring-boot-java8-examples/src/main/java/org/microframework/java/design/handler/SystemConfigSourceHandler.java
+++ spring-boot-java8-examples/src/main/java/org/microframework/java/design/handler/SystemConfigSourceHandler.java
@@ -1,15 +1,15 @@
-package org.microframework.java.design.handler;
-
-import org.microframework.java.design.ConfigSourceHandler;
-
-/**
- * @author Shaoyu Liu
- * @date 2022-09-27
- */
-public class SystemConfigSourceHandler implements ConfigSourceHandler {
-    @Override
-    public String get(String key) {
-        System.out.println("SystemConfigSourceHandler.get");
-        return "SystemConfigSourceHandler.get";
-    }
-}
+//package org.microframework.java.design.handler;
+//
+//import org.microframework.java.design.ConfigSourceHandler;
+//
+///**
+// * @author Shaoyu Liu
+// * @date 2022-09-27
+// */
+//public class SystemConfigSourceHandler implements ConfigSourceHandler {
+//    @Override
+//    public String get(String key) {
+//        System.out.println("SystemConfigSourceHandler.get");
+//        return "SystemConfigSourceHandler.get";
+//    }
+//}
