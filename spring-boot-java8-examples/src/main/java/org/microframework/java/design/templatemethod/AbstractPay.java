45844348fe880b4058b6b99f7b66dad902631018
刘邵宇 <shaoyu.liu@qq.com>
2023-05-09 18:29:43 +0800

    设计模式：模板方法

--- spring-boot-java8-examples/src/main/java/org/microframework/java/design/templatemethod/AbstractPay.java
+++ spring-boot-java8-examples/src/main/java/org/microframework/java/design/templatemethod/AbstractPay.java
@@ -1,5 +1,18 @@
 package org.microframework.java.design.templatemethod;
 
 public abstract class AbstractPay {
+
     protected abstract void pay();
+
+    /**
+     * 模板方法流程1
+     */
+    public final void processMethod1() {
+    }
+
+    /**
+     * 模板方法流程2
+     */
+    public final void processMethod2() {
+    }
 }
