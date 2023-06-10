532258d107f1c29ddf8092cd1b0d06d26100a9eb
Shaoyu Liu <455028117@qq.com>
2023-06-10 23:03:37 +0800

    mybatis generate

---
+++ spring-boot-java8-examples/src/main/java/org/microframework/java/io/serializable/MySerializable.java
@@ -0,0 +1,11 @@
+package org.microframework.java.io.serializable;
+
+import java.io.Serializable;
+
+/**
+ * @author Shaoyu Liu
+ * @date 2022/12/30 23:02
+ */
+public class MySerializable implements Serializable {
+    
+}
