8366ec3d7262c815521cc6f6ba5f940a197ebb00
Jerry <455028117@qq.com>
2023-06-29 02:55:14 +0000

    function 出入参使用大全
    Signed-off-by: Jerry <455028117@qq.com>

---
+++ spring-boot-java8-examples/src/main/java/org/microframework/java/function/AllFunctionTest.java
@@ -0,0 +1,55 @@
+package com.bty.oa.organization.service.impl;
+
+import java.util.function.BiFunction;
+import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.function.BinaryOperator;
+import java.util.function.UnaryOperator;
+/**
+ * @author Shaoyu Liu
+ * @date 2023/6/29
+ */
+public class AllFunctionTest {
+    public static void main(String[] args) {
+        /* 一、基本函数
+           1、Function; BiFunction
+           2、Consumer; BiConsumer
+           3、Predicate; BiPredicate
+           4、Supplier */
+
+        /* 1.1 入参：对象；返回：对象R
+           1.2 入参：对象1，对象2；返回：对象R  */
+        Function<Object, Object> function = s -> null;
+        BiFunction<Object, Object, Object> biFunction = (s, t) -> null;
+
+        /* 2.1 入参：对象；返回：无
+           2.2 入参：对象1，对象2；返回：无 */
+        Consumer<Object> consumer = s -> {};
+        BiConsumer<Object, Object> biConsumer = (s, t) -> {};
+
+        /* 3.1 入参：对象；返回：布尔值
+           3.2 入参：对象1，对象2；返回：布尔值 */
+        Predicate<Object> predicate = s -> true;
+        BiPredicate<Object, Object> biPredicate = (s, t) -> true;
+
+        /* 4.1 入参：方法引用；返回：无 ( this::getObjectMethod)*/
+        Supplier<Object> supplier = Object::new;
+
+
+
+        /* 二、进阶函数
+           1、UnaryOperator 继承 Function
+           2、BinaryOperator 继承  BiFunction  */
+
+        /* 1. 入参：对象；返回：原对象 */
+        UnaryOperator<Object> unaryOperator = s -> s;
+
+        /* 1. 入参：对象1，对象2；返回：对象1或对象2  */
+        BinaryOperator<Object> binaryOperator = (s, t) -> {  return s;};
+
+    }
+}
